b3e98ddbe07b2b57bfd68737ac05712e724a6a5a
flothesof <florian.s.lebourdais@gmail.com>
2020-04-15 11:50:40 +0200

    fix scrolling issues during modal fullscreen

--- script.js
+++ script.js
@@ -41,6 +41,51 @@ function randInt(N) {
     return Math.floor(Math.random() * N);
 }
 
+/* disable scrolling helper functions based on https://stackoverflow.com/questions/4770025/how-to-disable-scrolling-temporarily */
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function() { supportsPassive = true; }
+    }));
+} catch (e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+
 function updateQuizzProgressBar() {
     // updates progress bar
     var quizzData = document.getElementById('game-area').quizzData;
@@ -127,6 +172,7 @@ function toggleGameAreaModal() {
         // greater than -1; so 'theWall' is 'open', so here
         // we close it:
         gameArea.className = gameArea.className.replace('open', '');
+        enableScroll();
     } else {
         // the else here means that the string was not found,
         // returning an index of -1 (or, technically, -1 or less;
@@ -134,6 +180,7 @@ function toggleGameAreaModal() {
         // so the string was not found, means the 'theWall' is
         // 'closed' and so must be opened:
         gameArea.className = gameArea.className + ' open';
+        disableScroll();
     }
 }
 
