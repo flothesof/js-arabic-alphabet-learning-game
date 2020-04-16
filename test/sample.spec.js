487ea1343dc685efa260481f4b21288bcef4ffd5
flothesof <florian.s.lebourdais@gmail.com>
2020-04-16 13:34:02 +0200

    write tests

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -4,63 +4,62 @@ const serve = require("koa-static");
 const puppeteer = require("puppeteer");
 const { expect } = require("chai");
 
-describe("sample test", function () {
-  let browser;
-  let server;
-  let page;
+describe("UI test suite for alphabet game", function() {
+    let browser;
+    let server;
+    let page;
 
-  before(async function () {
-    this.timeout(10000);
-    const app = new Koa();
-    app.use(serve("."));
-    server = http.createServer(app.callback());
-    server.listen(3000);
+    before(async function() {
+        this.timeout(10000);
+        const app = new Koa();
+        app.use(serve("."));
+        server = http.createServer(app.callback());
+        server.listen(3000);
 
-    browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
+        browser = await puppeteer.launch({
+            headless: true,
+            defaultViewport: null,
+        });
     });
-  });
 
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("http://localhost:3000");
-  });
-
-  afterEach(async () => {
-    await page.close();
-  });
-
-  after(async function () {
-    await browser.close();
-    await new Promise((resolve) => {
-      server.close(resolve);
+    beforeEach(async() => {
+        page = await browser.newPage();
+        await page.goto("http://localhost:3000");
     });
-  });
 
-  it("should have the correct page title", async function () {
-    console.log(page.title());
-    expect(await page.title()).to.eql("Puppeteer Mocha");
-  });
+    afterEach(async() => {
+        await page.close();
+    });
 
-  it("should have a heading", async function () {
-    const HEADING_SELECTOR = "h1";
-    let heading;
+    after(async function() {
+        await browser.close();
+        await new Promise((resolve) => {
+            server.close(resolve);
+        });
+    });
 
-    await page.waitFor(HEADING_SELECTOR);
-    heading = await page.$eval(
-      HEADING_SELECTOR,
-      (heading) => heading.innerText
-    );
+    it("should have the correct page title", async function() {
+        expect(await page.title()).to.eql("Apprendre l'alphabet arabe !");
+    });
 
-    expect(heading).to.eql("Page Title");
-  });
+    it("should have a heading", async function() {
+        const HEADING_SELECTOR = "h1";
+        let heading;
 
-  it("should have a single content section", async function () {
-    const BODY_SELECTOR = ".main-content";
+        await page.waitFor(HEADING_SELECTOR);
+        heading = await page.$eval(
+            HEADING_SELECTOR,
+            (heading) => heading.innerText
+        );
 
-    await page.waitFor(BODY_SELECTOR);
+        expect(heading).to.eql("Apprendre l'alphabet arabe !");
+    });
 
-    expect(await page.$$(BODY_SELECTOR)).to.have.lengthOf(1);
-  });
+    it("should have a single content section", async function() {
+        const BODY_SELECTOR = ".modal";
+
+        await page.waitFor(BODY_SELECTOR);
+
+        expect(await page.$$(BODY_SELECTOR)).to.have.lengthOf(1);
+    });
 });
