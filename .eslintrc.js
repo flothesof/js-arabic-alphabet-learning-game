957b898520e2f1c5b3b628fad3edcb73072486ff
Rémi Berson <remi@cliqz.com>
2020-04-16 12:34:03 +0200

    Fix test infra

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,8 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es6": true
+        "browser": true
     },
     "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
--- test/sample.spec.js
+++ test/sample.spec.js
@@ -1,35 +1,66 @@
-describe('sample test', function() {
-    let page;
+const Koa = require("koa");
+const http = require("http");
+const serve = require("koa-static");
+const puppeteer = require("puppeteer");
+const { expect } = require("chai");
 
-    before(async function() {
-        page = await browser.newPage();
-        await page.goto('http://localhost:8080');
+describe("sample test", function () {
+  let browser;
+  let server;
+  let page;
+
+  before(async function () {
+    this.timeout(10000);
+    const app = new Koa();
+    app.use(serve("."));
+    server = http.createServer(app.callback());
+    server.listen(3000);
+
+    browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
     });
+  });
 
-    after(async function() {
-        await page.close();
-    })
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000");
+  });
 
-    it('should have the correct page title', async function() {
-        console.log(page.title());
-        expect(await page.title()).to.eql('Puppeteer Mocha');
+  afterEach(async () => {
+    await page.close();
+  });
+
+  after(async function () {
+    await browser.close();
+    await new Promise((resolve) => {
+      server.close(resolve);
     });
+  });
 
-    it('should have a heading', async function() {
-        const HEADING_SELECTOR = 'h1';
-        let heading;
+  it("should have the correct page title", async function () {
+    console.log(page.title());
+    expect(await page.title()).to.eql("Puppeteer Mocha");
+  });
 
-        await page.waitFor(HEADING_SELECTOR);
-        heading = await page.$eval(HEADING_SELECTOR, heading => heading.innerText);
+  it("should have a heading", async function () {
+    const HEADING_SELECTOR = "h1";
+    let heading;
 
-        expect(heading).to.eql('Page Title');
-    });
+    await page.waitFor(HEADING_SELECTOR);
+    heading = await page.$eval(
+      HEADING_SELECTOR,
+      (heading) => heading.innerText
+    );
 
-    it('should have a single content section', async function() {
-        const BODY_SELECTOR = '.main-content';
+    expect(heading).to.eql("Page Title");
+  });
 
-        await page.waitFor(BODY_SELECTOR);
+  it("should have a single content section", async function () {
+    const BODY_SELECTOR = ".main-content";
 
-        expect(await page.$$(BODY_SELECTOR)).to.have.lengthOf(1);
-    });
+    await page.waitFor(BODY_SELECTOR);
+
+    expect(await page.$$(BODY_SELECTOR)).to.have.lengthOf(1);
+  });
 });
